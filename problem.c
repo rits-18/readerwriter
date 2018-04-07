fa58ccbd85282d90d9f0d190bed8ece091506704
rits-18 <32608710+rits-18@users.noreply.github.com>
2018-04-08 01:05:16 +0530

    Update problem.c

--- problem.c
+++ problem.c
@@ -39,3 +39,39 @@ void *Reader(void *arg)
   }
   sem_post(&rcAccess);
 }
+int main()
+{
+  int i,rt,wt;
+  sem_init(&rcAccess,0,1);
+  sem_init(&dbAccess,0,1);
+  pthread_t Readers_thr[100],Writer_thr[100];
+  printf("\nEnter number of Readers thread");
+  scanf("%d",&rt);
+  printf("\nEnter number of Writers thread");
+  scanf("%d",&wt);
+  for(i=0;i<rt;i++)
+  {
+      if(pthread_create(&Readers_thr[i],NULL,Reader,(void *)i) != 0)
+	    {
+		      fprintf(stderr, "Error while creating Reader's thread\n");
+		      exit(1);
+	     }
+  }
+  for(i=0;i<wt;i++)
+  {
+      if(pthread_create(&Writer_thr[i],NULL,Writer,(void *)i); != 0)
+	    {
+		      fprintf(stderr, "Error while creating writer's thread\n");
+		      exit(1);
+	    }
+  }
+  for(i=0;i<wt;i++)
+  {
+	    pthread_join(Writer_thr[i],NULL);
+   }
+   for(i=0;i<rt;i++)
+   {
+	    pthread_join(Readers_thr[i],NULL);
+   }
+   return 0;
+} 
