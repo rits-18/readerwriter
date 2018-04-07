f4d154919500a709824d43f5a35244fd618fc003
rits-18 <32608710+rits-18@users.noreply.github.com>
2018-04-08 00:46:09 +0530

    Update problem.c

--- problem.c
+++ problem.c
@@ -8,11 +8,34 @@ int readCount=0;
 
 void *Writer(void *arg)
 {
-sleep(1);
-int temp=(int)arg;
-printf("\nWriter %d is trying to enter into database for modifying the data",temp);
-sem_wait(&databaseAccess);
-printf("\nWriter %d is writting into the database",temp);
-printf("\nWriter %d is leaving the database");
-sem_post(&databaseAccess);
+  sleep(1);
+  int temp=(int)arg;
+  printf("\nWriter %d is trying to enter into database for modifying the data",temp);
+  sem_wait(&databaseAccess);
+  printf("\nWriter %d is writting into the database",temp);
+  printf("\nWriter %d is leaving the database");
+  sem_post(&databaseAccess);
+}
+
+void *Reader(void *arg)
+{
+  sleep(1);
+  int temp=(int)arg;
+  printf(“\nReader %d is trying to enter into the Database for reading the data”,temp);
+  sem_wait(&readCountAccess);
+  readCount++;
+  if(readCount==1)
+  {
+    sem_wait(&databaseAccess);
+    printf(“\nReader %d is reading the database”,temp);
+  }
+  sem_post(&readCountAccess);
+  sem_wait(&readCountAccess);
+  readCount–;
+  if(readCount==0)
+  {
+    printf(“\nReader %d is leaving the database”,temp);
+    sem_post(&databaseAccess);
+  }
+  sem_post(&readCountAccess);
 }
